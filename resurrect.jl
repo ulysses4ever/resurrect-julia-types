f8fdee4a809ea9f5179c3d091c4cdc2711f27f41
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2023-04-05 13:46:35 -0400

    getting there

--- resurrect.jl
+++ resurrect.jl
@@ -3,12 +3,18 @@
 # E.g.: JULIA_DEPOT_PATH=. julia resurrect.jl
 #
 
-using CSV, Pkg # DataFrames, Query
+@info "Starting resurrect.jl. Using packages..."
+
+using CSV, Pkg, DataFrames #, Query
+
+@info "... done."
 
 parts(ty) = split(ty,".")
 
-unqualified_type(ty) = last(parts(ty))
-root_module(ty) = first(parts(ty))
+unqualified_type(ty) = string(last(parts(ty)))
+root_module(ty) = string(first(parts(ty)))
+
+evalp(s::String) = eval(Meta.parse(s))
 
 """
 addpackage: (tyrow : {modl, tyname, occurs}) -> IO ()
@@ -16,25 +22,55 @@ addpackage: (tyrow : {modl, tyname, occurs}) -> IO ()
 The function tries to update the current environment in a way that it's possible to
 `Core.eval(tyrow.modl, tyrow.tyname)`.
 """
-addpackage(tyrow) = begin
-    tyrow.modl in ["Core", "Base"] && return true
+addpackage(tyRow) = begin
+    tyRow.modl in ["Core", "Base"] && return true
 
     try
-        Pkg.add(root_module(ty.name))
-    catch e
-        @warn "Couldn't add package for type $ty"
+        rm=root_module(tyRow.tyname)
+        @info "Try to Pkg.add package $(rm)... (may take some time)"
+        Pkg.add(rm;io=devnull)
+        @info "... done"
+    catch err
+        @warn "Couldn't add package for type $(tyRow.tyname) (module: $(tyRow.modl))"
+        errio=stderr
+        showerror(errio, err)
+        println(errio)
         return false
     end
 
     return true
 end
 
-intypesCsvFile = "all-types-merged.csv"
-intypesCsv = CSV.read(intypesCsvFile, DataFrame)
-for tyRow in eachrow(intypesCsv)
-    print(tyRow.tyname)
-    if addpackage(tyRow)
-        ty = Core.eval(tyRow.modl, unqualified_type(tyRow.tyname))
-        print(ty)
+main() = begin
+    @info "Reading in data..."
+
+    intypesCsvFile = "all-types-merged.csv"
+    intypesCsv = CSV.read(intypesCsvFile, DataFrame)
+
+    @info "... done."
+
+    failed=[]
+    i=0
+    fi=0
+    gi=0
+    for tyRow in eachrow(intypesCsv)
+
+        # Special cases (skip for now):
+        # - function types
+        startswith(tyRow.tyname, "typeof") && (fi += 1; continue)
+        # - generic types
+        '{' in tyRow.tyname && (gi += 1; continue)
+
+        i+=1
+        @info "[$i] Processing: $(tyRow.tyname) from $(tyRow.modl)"
+
+        if addpackage(tyRow)
+            evalp("using $(tyRow.modl)")
+            m = evalp(tyRow.modl)
+            ty = Core.eval(m, unqualified_type(tyRow.tyname))
+        else
+            push!(failed, tyRow)
+        end
     end
+    (i,fi,gi)
 end
